7a94bf17600816f19111b7374753088a55015cb1
François MATHIEU <francois.mathieu@livexp.fr>
2018-10-03 12:26:09 +0200

    Add content to the form

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +12,9 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title');
+            ->add('title')
+            ->add('content')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)

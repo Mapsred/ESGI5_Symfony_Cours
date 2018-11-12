f916de5cba5df358487789748454d0c8e1c8f0a6
François MATHIEU <maps_red@yahoo.fr>
2018-11-12 08:58:30 +0100

    Fix redirect

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -2,15 +2,17 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ORM\Table(name="tag")
  * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
  */
 class Tag
 {
     /**
-     * @var integer $id
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
@@ -22,6 +24,20 @@ class Tag
      * @ORM\Column(type="string", length=255)
      */
     private $name;
+
+    /**
+     * @var ArrayCollection $articles
+     * @ORM\ManyToMany(targetEntity="App\Entity\Article", mappedBy="tags")
+     */
+    private $articles;
+
+    /**
+     * Tag constructor.
+     */
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
 
     /**
      * @return int|null
@@ -48,5 +64,49 @@ class Tag
         $this->name = $name;
 
         return $this;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    /**
+     * @param Article $article
+     * @return Tag
+     */
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->addTag($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Article $article
+     * @return Tag
+     */
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->contains($article)) {
+            $this->articles->removeElement($article);
+            $article->removeTag($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function __toString()
+    {
+        return $this->getName();
     }
 }
